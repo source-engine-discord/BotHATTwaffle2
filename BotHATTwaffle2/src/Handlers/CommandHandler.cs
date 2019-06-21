cca7c86507bb55480127bff26dec12030a379a6e
18swenskiq <18swenskiq@gmail.com>
2019-06-21 18:49:47 -0400

    Now listens for some specific questions/statements as defined in settings.json

--- BotHATTwaffle2/src/Handlers/CommandHandler.cs
+++ BotHATTwaffle2/src/Handlers/CommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using BotHATTwaffle2.Services;
 using BotHATTwaffle2.Services.Steam;
 using BotHATTwaffle2.src.Handlers;
 using BotHATTwaffle2.TypeReader;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 
@@ -138,12 +140,82 @@ namespace BotHATTwaffle2.Handlers
         /// <returns></returns>
         internal async void Listen(SocketMessage message)
         {
+            // Don't want to listen to what a bot tells us to do
             if (message.Author.IsBot) return;
+
+            // Embed Steam workshop links
             if ((message.Content.Contains("://steamcommunity.com/sharedfiles/filedetails/?id=")) || (message.Content.Contains("://steamcommunity.com/workshop/filedetails/")))
             {
                 // The two empty strings here are for image album and test type (for when the bot sends the "playtest submitted" message)
                 await _workshop.HandleWorkshopEmbeds(message, _data);
+                return;
             }
+
+            // Listen for specific user questions, then answer them if we can
+
+            // Listen for carve messages
+            if (_data.RSettings.AutoReplies.Carve.Any(s => message.Content.ToLower().Contains(s)))
+            {
+                var carveEmbed = new EmbedBuilder()
+                    .WithAuthor($"Hey there {message.Author.Username}!", message.Author.GetAvatarUrl())
+                    .WithTitle($"DO NOT USE CARVE!")
+                    .WithThumbnailUrl("https://i.ytimg.com/vi/xh9Kr2iO4XI/maxresdefault.jpg")
+                    .WithDescription("You were asking about carve. We don't use carve here. Not only does it create bad brushwork, but it " +
+                                     "can also cause Hammer to stop responding and crash. If you're here trying to defend using carve, just stop - you are wrong.")
+                    .WithColor(new Color(243, 128, 72));
+
+                await message.Channel.SendMessageAsync(embed: carveEmbed.Build());
+            }
+
+            // Listen for packing questions
+            if (_data.RSettings.AutoReplies.Packing.Any(s => message.Content.ToLower().Contains(s)))
+            {
+                var packingEmbed = new EmbedBuilder()
+                    .WithAuthor($"Hey there {message.Author.Username}!", message.Author.GetAvatarUrl())
+                    .WithTitle($"Click here to learn how to use VIDE!")
+                    .WithUrl("https://www.tophattwaffle.com/packing-custom-content-using-vide-in-steampipe/")
+                    .WithThumbnailUrl("https://www.tophattwaffle.com/wp-content/uploads/2013/11/vide.png")
+                    .WithDescription("I noticed you may be looking for information on how to pack custom content into your level. " +
+                                     "This is easily done using VIDE. Click the link above to download VIDE and learn how to use it.")
+                    .WithColor(new Color(243, 128, 72));
+
+                await message.Channel.SendMessageAsync(embed: packingEmbed.Build());
+            }
+
+            // Tell users that pakrat is bad
+            if (_data.RSettings.AutoReplies.Pakrat.Any(s => message.Content.ToLower().Contains(s)))
+            {
+                var pakratEmbed = new EmbedBuilder()
+                    .WithAuthor($"Hey there {message.Author.Username}!", message.Author.GetAvatarUrl())
+                    .WithTitle($"Click here to learn how to use VIDE!")
+                    .WithUrl("https://www.tophattwaffle.com/packing-custom-content-using-vide-in-steampipe/")
+                    .WithThumbnailUrl("https://www.tophattwaffle.com/wp-content/uploads/2013/11/vide.png")
+                    .WithDescription("I was minding my own business when I heard you mention something about PakRat. " +
+                                     "Don't know if you know this, but PakRat is super old and has been know to cause issues in newer games. " +
+                                     "There is a newer program that handles packing better called VIDE. You should check that out instead.")
+                    .WithColor(new Color(243, 128, 72));
+
+                await message.Channel.SendMessageAsync(embed: pakratEmbed.Build());
+                
+            }
+
+            // Recommend WallWorm over propper
+            if (_data.RSettings.AutoReplies.Propper.Any(s => message.Content.ToLower().Contains(s)))
+            {
+                var wallWormEmbed = new EmbedBuilder()
+                    .WithAuthor($"Hey there {message.Author.Username}!", message.Author.GetAvatarUrl())
+                    .WithTitle($"Click here to go to the WallWorm site!")
+                    .WithUrl("https://dev.wallworm.com/")
+                    .WithThumbnailUrl("https://www.tophattwaffle.com/wp-content/uploads/2017/12/worm_logo.png")
+                    .WithDescription("I saw you were asking about propper. While Propper still works, it's advised to learn " +
+                                     "a better modeling solution. The preferred method for Source Engine is using 3dsmax with WallWorm Model Tools" +
+                                     " If you don't want to learn 3dsmax and WWMT, you can learn to configure propper at the link below.: " +
+                                     "\n\nhttps://www.tophattwaffle.com/configuring-propper-for-steampipe/")
+                    .WithColor(new Color(243, 128, 72));
+
+                await message.Channel.SendMessageAsync(embed: wallWormEmbed.Build());
+            }
+
             //Add code here for eavesdropping
         }
     }
