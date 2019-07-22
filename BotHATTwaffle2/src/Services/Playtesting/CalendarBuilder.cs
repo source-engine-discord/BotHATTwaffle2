405768d0812ef799fc9dc3c3674890449fe470aa
18swenskiq <18swenskiq@gmail.com>
2019-07-22 01:25:20 -0400

    Moved some stuff around in the calendar layout, as well as added more control variables at the top of the file

--- BotHATTwaffle2/src/Services/Playtesting/CalendarBuilder.cs
+++ BotHATTwaffle2/src/Services/Playtesting/CalendarBuilder.cs
@@ -19,21 +19,21 @@ namespace BotHATTwaffle2.Services.Playtesting
             // Gonna just yeet out of here if there are no playtests
             if (calPlaytestEvents.Items.Count == 0) return;
 
-            // Dimensions of our image to make
-            int width = 1280;
-            int height = 720;
+            // Here's a bunch of stuff you can easily change
+            int width = 1000;
+            int height = 800;
+            Rgba32 dateColor = Rgba32.Black;
+            Rgba32 playtestTitleColor = Rgba32.GhostWhite;
+            Rgba32 playtestTimeColor = Rgba32.Black;
+            Rgba32 backgroundColor = new Rgba32(.21176f, .22353f, .24706f); // This is Discord's dark theme's exact background color
+            Rgba32 lineColor = Rgba32.Black;
+            string playtestDateFontName = "Arial";
+            string playtestMapFontName = "Arial";
+            string playtestTimeFontName = "Arial";
+            // Modifying anything besides these variables may cause instability, so be extremely cautious if you do so
 
-
-            // WARNING: Exercise caution in changing any of the (dimension * float) values, as that means the value is procedurally generated from the height or width
             using (Image<Rgba32> image = new Image<Rgba32>(width, height))
             {
-
-                // Change colors here
-                Rgba32 dateColor = Rgba32.Black;
-                Rgba32 playtestTitleColor = Rgba32.GhostWhite;
-                Rgba32 playtestTimeColor = Rgba32.Black;
-                Rgba32 backgroundColor = new Rgba32(.21176f, .22353f, .24706f); // This is Discord's dark theme's exact background color
-                Rgba32 lineColor = Rgba32.Black;
 
                 // Changes background color
                 image.Mutate(x => x
@@ -56,27 +56,28 @@ namespace BotHATTwaffle2.Services.Playtesting
                     .Draw(calLinePen, new RectangleF((widthF - 1f), 0f, 1f, heightF)) // Vertical: 8 (Far Right) (This one should be weight - 1 for the first value)
 
                     .Draw(calLinePen, new RectangleF(0f, 0f, widthF, 1f)) // Horizontal: 1 (Top) (Thin)
-                    .Draw(calLinePen, new RectangleF(0f, (heightF * .06141f), widthF, 2f)) // Horizonal: 2 (Thick)
+                    .Draw(calLinePen, new RectangleF(0f, (heightF * .03141f), widthF, 0f)) // Horizonal: 2 (Thick)
                     .Draw(calLinePen, new RectangleF(0f, (heightF * .20046f), widthF, 0f)) // Horizonal: 3 (Thin)
-                    .Draw(calLinePen, new RectangleF(0f, (heightF * .26072f), widthF, 2f)) // Horizonal: 4 (Thick)
+                    .Draw(calLinePen, new RectangleF(0f, (heightF * .23072f), widthF, 0f)) // Horizonal: 4 (Thick)
                     .Draw(calLinePen, new RectangleF(0f, (heightF * .39977f), widthF, 0f)) // Horizonal: 5 (Thin)
-                    .Draw(calLinePen, new RectangleF(0f, (heightF * .46002f), widthF, 2f)) // Horizontal: 6 (Thick)
+                    .Draw(calLinePen, new RectangleF(0f, (heightF * .43002f), widthF, 0f)) // Horizontal: 6 (Thick)
                     .Draw(calLinePen, new RectangleF(0f, (heightF * .59907f), widthF, 0f)) // Horizontal: 7 (Thin)
-                    .Draw(calLinePen, new RectangleF(0f, (heightF * .65933f), widthF, 2f)) // Horizonal: 8 (Thick)
+                    .Draw(calLinePen, new RectangleF(0f, (heightF * .62933f), widthF, 0f)) // Horizonal: 8 (Thick)
                     .Draw(calLinePen, new RectangleF(0f, (heightF * .79838f), widthF, 0f)) // Horizontal: 9 (Thin)
-                    .Draw(calLinePen, new RectangleF(0f, (heightF * .85863f) , widthF, 2f)) // Horizontal: 10 (Thick)
+                    .Draw(calLinePen, new RectangleF(0f, (heightF * .82863f) , widthF, 0f)) // Horizontal: 10 (Thick)
                     .Draw(calLinePen, new RectangleF(0f, (heightF - 1f), widthF, 1f)) // Horizontal: 11 (Bottom) (Thin) (The second value should be height - 1)
                 );
 
                 // Getting the current time (start of calendar) and setting up our fonts
+
+                var fontDates = SystemFonts.CreateFont(playtestDateFontName, 18, FontStyle.Bold);
+                var fontPTName = SystemFonts.CreateFont(playtestMapFontName, 20, FontStyle.Regular);
+                var fontPT = SystemFonts.CreateFont(playtestTimeFontName, 23, FontStyle.Regular);
                 var currentDateTime = DateTime.Now;
-                var fontDates = SystemFonts.CreateFont("Arial", (23.536f * (width/height)), FontStyle.Bold);
-                var fontPTName = SystemFonts.CreateFont("Arial", (12.195f * (width/height)), FontStyle.Regular);
-                var fontPT = SystemFonts.CreateFont("Arial", (14.025f * (width/height)), FontStyle.Regular);
 
                 // Here are the coordinate lists for the date headings (and xPos is also used by the playtest events)
                 List<float> xPosList = new List<float>() { (widthF * .07148f), (widthF * .21384f), (widthF * .35667f), (widthF * .49854f), (widthF * .64114f), (widthF * .78337f), (widthF * .92706f) };
-                List<float> yPosList = new List<float>() { (heightF * .01196f), (heightF * .21052f), (heightF * .41029f), (heightF * .60766f), (heightF * .80909f) };
+                List<float> yPosList = new List<float>() { (heightF * .00796f), (heightF * .20652f), (heightF * .40629f), (heightF * .60366f), (heightF * .80509f) };
 
                 // Need to set up these variables here for scoping reasons
                 SizeF customSize;
@@ -99,8 +100,8 @@ namespace BotHATTwaffle2.Services.Playtesting
 
                 // These are similar to the date headings, but these are for the playtest names and times
                 // They are in groups of 2. So each set of 2 is in the same row
-                List<float> playtestTitlesYPosList = new List<float>() { (heightF * .06998f),  (heightF * .13696f),   (heightF * .26914f),  (heightF * .33612f),   (heightF * .46830f), (heightF * .53529f),    (heightF * .66746f), (heightF * .73445f),   (heightF * .86663f), (height * .93361f),  };
-                List<float> playtestTimesYPosList = new List<float>()  { (heightF * .09689f),  (heightF * .16388f),   (heightF * .29605f),  (heightF * .36304f),   (heightF * .49522f), (heightF * .56220f),    (heightF * .69438f), (heightF * .76136f),   (heightF * .89354f), (height * .96232f),  };
+                List<float> playtestTitlesYPosList = new List<float>() { (heightF * .04998f),  (heightF * .12696f),   (heightF * .24914f),  (heightF * .32615f),   (heightF * .44833f), (heightF * .52531f),    (heightF * .64749f), (heightF * .72447f),   (heightF * .84665f), (height * .92363f),  };
+                List<float> playtestTimesYPosList = new List<float>()  { (heightF * .07689f),  (heightF * .15388f),   (heightF * .27605f),  (heightF * .35304f),   (heightF * .47522f), (heightF * .55220f),    (heightF * .67438f), (heightF * .75136f),   (heightF * .87354f), (height * .95232f),  };
 
                 // Here's the big boi. Putting the playtest events on the calendar
                 List<int> numOfPlaytests = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
