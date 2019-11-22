92c63ee659195bdbf6a177c525eb9fab67da5585
MarkKoz <KozlovMark@gmail.com>
2019-11-21 20:31:15 -0800

    FaceItModule: use command groups
    * Make module a group named "FaceIt".
    * Make a sub-group named "Tags" for tag commands and split commands
      into separate functions.
    * Rename command functions.
    * Rename GetHubFiles command to GetDemos.

--- BotHATTwaffle2/src/Commands/FaceItModule.cs
+++ BotHATTwaffle2/src/Commands/FaceItModule.cs
@@ -14,6 +14,7 @@ using Discord.Commands;
 
 namespace BotHATTwaffle2.Commands
 {
+    [Group("FaceIt")]
     public class FaceItModule : ModuleBase<SocketCommandContext>
     {
         private readonly DataService _dataService;
@@ -25,12 +26,12 @@ namespace BotHATTwaffle2.Commands
             _log = log;
         }
 
-        [Command("GetHubFiles", RunMode = RunMode.Async)]
+        [Command("GetDemos", RunMode = RunMode.Async)]
         [RequireUserPermission(GuildPermission.BanMembers)]
         [Summary("Invokes a fetch of games from all FaceIT hubs")]
-        [Remarks("`>GetHubFiles [startTime] [endTime]`" +
-                 "\nExample: `>GetHubFiles \"11/20/2019\" \"12/20/2019\"`")]
-        public async Task GetFaceItHubRangeAsync([Optional] string startTime, [Optional] string endTime)
+        [Remarks("`>FaceIt GetDemos [startTime] [endTime]`" +
+                 "\nExample: `>FaceIt GetDemos \"11/20/2019\" \"12/20/2019\"`")]
+        public async Task GetDemosAsync([Optional] string startTime, [Optional] string endTime)
         {
             var embed = new EmbedBuilder()
                 .WithColor(55, 55, 165)
@@ -52,63 +53,16 @@ namespace BotHATTwaffle2.Commands
             await message.ModifyAsync(x => x.Embed = embed.Build());
         }
 
-        [Command("HubTags")]
-        [RequireUserPermission(GuildPermission.BanMembers)]
-        [Summary("Manages tags for FaceIt hub demo handing")]
-        [Remarks("Dates should **NOT** overlap. Make sure the ending date it 23:59 as well." +
-                 "\n`>HubTags show` will display all current tags, sorted by date." +
-                 "\n`>HubTags delete [index]` will remove a hub tag from the list. Use `show` to see indexes." +
-                 "\n`>HubTags add [Traditional/Wingman] [tagName] \"[startDate]\" \"[endDate]\"" +
-                 "\nExample: `>HubTags Traditional MCHs9 \"11/20/2019 00:00\" \"12/20/2019 23:59\"" +
-                 "\nQuotes are required around the dates")]
-        public async Task FaceItHubTagsAsync(string command, [Optional] string type, [Optional] string tagName,
-            [Optional] string startTime, [Optional] string endTime)
-        {
-            var embed = new EmbedBuilder()
-                .WithColor(55, 55, 165);
+        [Group("Tags")]
+        class FaceItTagsModule : ModuleBase<SocketCommandContext> {
+            [Command("Add")]
+            [RequireUserPermission(GuildPermission.BanMembers)]
+            [Summary("Add a new FACEIT hub tag.")]
+            [Remarks("Dates should **NOT** overlap. Make sure the ending date is 23:59 as well.")]
+            public async Task AddAsync(string type, string tagName, string startTime, string endTime) {
+                var embed = new EmbedBuilder()
+                    .WithColor(55, 55, 165);
 
-            switch (command.ToLower())
-            {
-                case "show":
-                    Show();
-                    break;
-                case "add":
-                    Add();
-                    break;
-                case "delete":
-                    Delete();
-                    break;
-                default:
-                    embed.WithAuthor("Unknown Command!");
-                    embed.WithColor(165, 55, 55);
-                    break;
-            }
-
-            await ReplyAsync(embed: embed.Build());
-
-            //Handles showing the current entries
-            void Show()
-            {
-                //Get all items, sort by date, and reverse so it is newest first
-                var result = DatabaseUtil.GetHubTypes().OrderBy(x => x.EndDate).Reverse();
-                embed.WithAuthor("Current Faceit Hub Tags - Sorted most recent first");
-                var counter = 0;
-
-                foreach (var r in result)
-                {
-                    counter++;
-                    embed.AddField($"[{r.Id}] `{r.StartDate:MM/dd/yyyy HH:mm:ss} - {r.EndDate:MM/dd/yyyy HH:mm:ss}`",
-                        $"Type: `{r.Type}`" +
-                        $"\nTag: `{r.TagName}`");
-                    //Handle embed field limit
-                    if (counter >= 24)
-                        break;
-                }
-            }
-
-            //Handles adding a new entry
-            void Add()
-            {
                 if (type == null || tagName == null || startTime == null || endTime == null)
                 {
                     embed.WithAuthor("All parameters are required when adding a new hub tag!");
@@ -145,11 +99,17 @@ namespace BotHATTwaffle2.Commands
 
                 embed.WithAuthor("Failure adding hub tags!");
                 embed.WithColor(165, 55, 55);
+
+                await ReplyAsync(embed: embed.Build());
             }
 
-            //Handles deletion of a entry
-            void Delete()
-            {
+            [Command("Delete")]
+            [RequireUserPermission(GuildPermission.BanMembers)]
+            [Summary("Delete a FACEIT hub tag.")]
+            public async Task DeleteAsync(string type) {
+                var embed = new EmbedBuilder()
+                    .WithColor(55, 55, 165);
+
                 var wasDeleted = false;
                 if (type != null && int.TryParse(type, out var id))
                 {
@@ -170,6 +130,34 @@ namespace BotHATTwaffle2.Commands
                 }
 
                 embed.WithAuthor($"Result of tag deletion {wasDeleted}");
+
+                await ReplyAsync(embed: embed.Build());
+            }
+
+            [Command("Show")]
+            [RequireUserPermission(GuildPermission.BanMembers)]
+            [Summary("Show all current FACEIT hub tags sorted by date.")]
+            public async Task ShowAsync() {
+                var embed = new EmbedBuilder()
+                    .WithColor(55, 55, 165);
+
+                // Get all items, sort by date, and reverse so it is newest first
+                var result = DatabaseUtil.GetHubTypes().OrderBy(x => x.EndDate).Reverse();
+                embed.WithAuthor("Current Faceit Hub Tags - Sorted most recent first");
+                var counter = 0;
+
+                foreach (var r in result)
+                {
+                    counter++;
+                    embed.AddField($"[{r.Id}] `{r.StartDate:MM/dd/yyyy HH:mm:ss} - {r.EndDate:MM/dd/yyyy HH:mm:ss}`",
+                        $"Type: `{r.Type}`" +
+                        $"\nTag: `{r.TagName}`");
+                    // Handle embed field limit
+                    if (counter >= 24)
+                        break;
+                }
+
+                await ReplyAsync(embed: embed.Build());
             }
         }
     }
