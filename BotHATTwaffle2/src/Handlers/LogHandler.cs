537cd81133fd2daa8be690c2f8b27c53abc1d208
Douglas Hoogland <tophattwaffle@gmail.com>
2020-04-10 02:39:31 -0500

    Disabled alert by default in log handler.

--- BotHATTwaffle2/src/Handlers/LogHandler.cs
+++ BotHATTwaffle2/src/Handlers/LogHandler.cs
@@ -27,7 +27,7 @@ namespace BotHATTwaffle2.Handlers
             if (msg.Exception == null)
                 Console.WriteLine(msg.ToString());
             else
-                await LogMessage(msg.ToString(prependTimestamp: false), alert: true, color: ConsoleColor.Red);
+                await LogMessage(msg.ToString(prependTimestamp: false), alert: false, color: ConsoleColor.Red);
         }
 
         public async Task LogMessage(string msg, bool channel = true, bool console = true, bool alert = false,
