32fadc5eb3a908b06dc367308109cff917497afb
gui <guihori2000@hotmail.com>
2021-07-07 18:37:00 -0400

    Added Google Safebrowsing api to check links
    Requires Google Api Key

--- BotHATTwaffle2/src/Handlers/BlacklistHandler.cs
+++ BotHATTwaffle2/src/Handlers/BlacklistHandler.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using BotHATTwaffle2.Models.LiteDB;
 using BotHATTwaffle2.Services;
 using BotHATTwaffle2.Util;
 using Discord;
 using Discord.WebSocket;
+using Google.Apis.Safebrowsing.v4;
+using Google.Apis.Safebrowsing.v4.Data;
+using Google.Apis.Services;
 
 namespace BotHATTwaffle2.Handlers
 {
@@ -29,14 +33,31 @@ namespace BotHATTwaffle2.Handlers
         public bool CheckBlacklist()
         {
             var blCheckResult = LevelOneBlacklistCheck();
-            
-            if (blCheckResult == null)
-                return false;
+
+            if (blCheckResult != null)
+            {
+                ApplyMute(blCheckResult);
+                return true;
+            }
 
             //TODO: Implement "Fuzzy" matching with regex as a "Level 2" check
 
-            ApplyMute(blCheckResult);
-            return true;
+            Regex rx = new Regex(@"(https?://)[^ ]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            MatchCollection matches = rx.Matches(_message.Content);
+
+            foreach (Match match in matches)
+            {
+                string check = CheckURL(match.Value).Result;
+                if (check != null)
+                {
+                    //_message.Channel.SendMessageAsync("url unsafe: " + check);
+                    MuteUnsafeURL();
+                    return true;
+                }
+            }
+
+
+            return false;
         }
 
         /// <summary>
@@ -45,13 +66,56 @@ namespace BotHATTwaffle2.Handlers
         /// <returns>Returned null if no match is found, else returns the matching word</returns>
         private Blacklist LevelOneBlacklistCheck()
         {
-            foreach (var blacklist in _blacklist)
+            foreach (Blacklist blacklist in _blacklist)
             {
                 if (_message.Content.Contains(blacklist.Word, StringComparison.OrdinalIgnoreCase))
                     return blacklist;
             }
             return null;
         }
+
+        private async Task<string> CheckURL(string url)
+        {
+            var service = new SafebrowsingService(new BaseClientService.Initializer
+            {
+                ApplicationName = "dotnet-client",
+                ApiKey = "" // add APIKEY here
+            });
+            if (service.ApiKey == "") return null; //remove this after adding key. (or not. doesn't matter)
+
+
+            var request = service.ThreatMatches.Find(new GoogleSecuritySafebrowsingV4FindThreatMatchesRequest()
+            {
+                Client = new GoogleSecuritySafebrowsingV4ClientInfo
+                {
+                    ClientId = "Dotnet-client",
+                    ClientVersion = "1.5.2"
+                },
+                ThreatInfo = new GoogleSecuritySafebrowsingV4ThreatInfo()
+                {
+                    ThreatTypes = new List<string> { "Malware", "Social_Engineering", "Unwanted_Software", "Potentially_Harmful_Application" },
+                    PlatformTypes = new List<string> { "Any_Platform" },
+                    ThreatEntryTypes = new List<string> { "URL" },
+                    ThreatEntries = new List<GoogleSecuritySafebrowsingV4ThreatEntry>
+                {
+                    new GoogleSecuritySafebrowsingV4ThreatEntry
+                    {
+                        Url = url
+                    }
+                }
+                }
+            });
+
+            var response = await request.ExecuteAsync();
+            if (response.Matches != null)
+            {
+                //returns only first threat
+                return response.Matches[0].ThreatType;
+            }
+            return null;
+
+        }
+
 
         private async void ApplyMute(Blacklist blacklist)
         {
@@ -83,6 +147,22 @@ namespace BotHATTwaffle2.Handlers
             await _dataService.MuteUser((SocketGuildUser)_message.Author,
                 TimeSpan.FromMinutes(blacklist.AutoMuteDuration),
                 $"BLACKLIST VIOLATION [{blacklist.Word}]", _message);
+        }
+
+        private async void MuteUnsafeURL()
+        {
+            //For now at least keep the mute at 0 mins since we really can't know how bad the URL is.
+            //Google returns the type of violation so we could change it in the future
+            var message = await _message.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithAuthor($"{_message.Author.Username}")
+                .WithDescription($"Your message has been deleted because it contained an unsafe URL." +
+                                    $"\nPlease ask a staff member if you're unsure why.")
+                .WithColor(new Color(165, 55, 55))
+                .Build());
+
+            await Task.Delay(10000);
+            await message.DeleteAsync();
+            return;
         }
     }
 }
